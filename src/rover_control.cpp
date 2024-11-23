048d4bb975f4c73130db697c5fcd6c180c54e979
Benjamin PELLIEUX <benjaminpellieux5@gmail.com>
2024-11-23 12:17:09 -0500

    Opti log system

--- src/rover_control.cpp
+++ src/rover_control.cpp
@@ -26,18 +26,26 @@ std::string Rover::getStatusAsString() {
     }
 }
 
-void Rover::updateStatusfromMove(Movement gesture, cv::Point new_target){
+void Rover::updateStatusfromMove(Movement gesture, cv::Point new_target) {
+    Status oldStatus = status; // Sauvegarder l'ancien statut
+    cv::Point oldTarget = target; // Sauvegarder l'ancien point cible
+
+    // Mettre à jour le statut en fonction du geste détecté
     switch (gesture) {
         case Movement::HANDS_UP: setStatus(Status::DANCE); break;
         case Movement::HAND_RIGHT: setStatus(Status::STOP); break;
         case Movement::HAND_LEFT: setStatus(Status::FOLLOW); break;
         case Movement::TILT_RIGHT: setStatus(Status::PARKING); break;
         case Movement::TILT_LEFT: setStatus(Status::ERROR); break;
-        default: status = status; break;
+        default: break; // Pas de changement
     }
+
     target = new_target;
 
-    std::cout<<"INFO: Status: " << getStatusAsString() 
-             << "\nTarget: x" << target.x 
-             << "y" << target.y << std::endl;
+    // Imprimer uniquement si le statut ou la cible a changé
+    if (status != oldStatus || target != oldTarget) {
+        std::cout << "INFO: Status: " << getStatusAsString() 
+                  << "\nTarget: x" << target.x 
+                  << " y" << target.y << std::endl;
+    }
 }
